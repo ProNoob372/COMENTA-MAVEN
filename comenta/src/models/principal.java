95637730dad412420eca3881d537918232e9251c
Fernando <Fernando@DESKTOP-HDRRK7M>
2024-05-27 10:37:36 -0300

    2

--- comenta/src/models/principal.java
+++ comenta/src/models/principal.java
@@ -3,7 +3,9 @@ package models;
 public class principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("teste de commit 27/05/2024");
+		
+		//codigo main do app comenta
 
 	}
 
