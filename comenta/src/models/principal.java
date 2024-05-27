ec9e4d8ffcf9a6f6e564d756d4c7f8bc06d3610d
Fernando <Fernando@DESKTOP-HDRRK7M>
2024-05-27 10:39:49 -0300

    Merge branch 'master' of https://github.com/ProNoob372/COMENTA-MAVEN.git

--- comenta/src/models/principal.java
+++ comenta/src/models/principal.java
@@ -3,9 +3,10 @@ package models;
 public class principal {
 
 	public static void main(String[] args) {
+
 		System.out.println("teste de commit 27/05/2024");
-		
-		//codigo main do app comenta
+
+		// codigo main do app comenta
 
 	}
 
